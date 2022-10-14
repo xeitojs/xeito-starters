5bf468512de238b762f40a17d1ca934dbe4ece7b
Samuel Bazaga <samuelbazaga@gmail.com>
2022-10-14 17:55:19 +0200

    feat(): Add proper initial blank template

--- templates/blank/src/app/pages/home/Home.tsx
+++ templates/blank/src/app/pages/home/Home.tsx
@@ -1,15 +1,34 @@
-import { Xeito, Component } from '@xeito/core';
+import { Xeito, Component, State } from '@xeito/core';
 import styles from './home.module.scss';
 
 @Component()
 export class HomePage {
+
+  @State() count = 0;
 
   constructor() {}
 
   render() {
     return (
       <div>
-        <p className={styles.redAccent}>Home works!</p>
+        <div className={styles.wrapper}>
+          <div className={styles.container}>
+            <h1>Xeito</h1>
+
+            <button 
+              className={styles.button}
+              onClick={() => this.count++}
+            >
+              Count is: {this.count}
+            </button>
+
+            <p>Edit <span className={styles.mono}>/app/pages/Home.tsx</span> to test livereload</p>
+            
+            <div className={styles.link}>
+              <a href="https://github.com/aerotoad/xeito" target="_blank" className={styles.link}>Xeito Github repo</a>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ templates/blank/src/app/router.ts
@@ -0,0 +1,7 @@
+import { createRouterConfig } from '@xeito/router';
+import { RouterConfig } from '@xeito/router';
+
+export routerConfig: RouterConfig = createRouterConfig([
+  path: '/',
+  component: ,
+]);
