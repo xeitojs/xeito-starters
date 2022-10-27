b9369955ef4791a5bb6f374179d65520b6537869
Samuel Bazaga <samuelbazaga@gmail.com>
2022-10-27 17:15:50 +0200

    style(): Replace double quotes with simple quotes

--- templates/blank/src/main.tsx
+++ templates/blank/src/main.tsx
@@ -3,4 +3,4 @@ import { Router } from '@xeito/router';
 import { routerConfig } from './router-config';
 import './styles.scss';
 
-Xeito.render(document.getElementById("app"), <Router routerConfig={routerConfig} />);
+Xeito.render(document.getElementById('app'), <Router routerConfig={routerConfig} />);
